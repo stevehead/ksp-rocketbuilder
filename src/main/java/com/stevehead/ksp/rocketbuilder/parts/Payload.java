44411a933ae8e2bff1f704003914fc5c6e1bcdf1
Steve Johnson <sjohnson0684@gmail.com>
2015-03-29 15:40:06 -0400

    Payload and propellant class creation.

---
+++ src/main/java/com/stevehead/ksp/rocketbuilder/parts/Payload.java
@@ -0,0 +1,29 @@
+package com.stevehead.ksp.rocketbuilder.parts;
+
+import com.stevehead.ksp.rocketbuilder.interfaces.Massable;
+
+/**
+ * Payload is a static mass object that is placed on top of a rocket.
+ * 
+ * @author Steve Johnson
+ */
+public class Payload implements Massable {
+	/**
+	 * The mass of the payload in kg.
+	 */
+	private final double mass;
+	
+	/**
+	 * @param mass 		mass in kg
+	 */
+	public Payload(double mass) {
+		this.mass = mass;
+	}
+	
+	/**
+	 * @return		mass in kg
+	 */
+	public double getMass() {
+		return mass;
+	}
+}
---
+++ src/main/java/com/stevehead/ksp/rocketbuilder/parts/Propellant.java
@@ -0,0 +1,38 @@
+package com.stevehead.ksp.rocketbuilder.parts;
+
+/**
+ * Propellants are the fuels used in KSP.
+ * 
+ * @author Steve Johnson
+ */
+public enum Propellant {
+	LIQUID_FUEL (0.005),
+	OXIDIZER (0.005),
+	SOLID_FUEL (0.0075),
+	MONOPROPELLANT (0.004),
+	XENON_GAS (0.0001);
+	
+	/**
+	 * The ratio of liquid fuel to oxidizer required for combustion.
+	 */
+	public static final double LIQUID_FUEL_OXIDIZER_RATIO = 9.0 / 11.0;
+	
+	/**
+	 * The density of the propellant. Units are currently unknown.
+	 */
+	private final double density;
+	
+	/**
+	 * @param density		the density
+	 */
+	Propellant(double density) {
+		this.density = density;
+	}
+	
+	/**
+	 * @return		the density
+	 */
+	public double getDensity() {
+		return density;
+	}
+}
