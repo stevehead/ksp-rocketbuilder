e59bfd693122200c53f7ed10bb7880477f0f6bc6
Steve Johnson <sjohnson0684@gmail.com>
2015-04-03 13:22:46 -0400

    BaseComponent tests.

---
+++ src/test/java/com/stevehead/ksp/rocketbuilder/rocket/BaseComponentTest.java
@@ -0,0 +1,28 @@
+package com.stevehead.ksp.rocketbuilder.rocket;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class BaseComponentTest {
+	
+	protected static BaseComponent testComponent;
+	private static final double mass = 100;
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		testComponent = new TestComponent(mass);
+	}
+	
+	@Test
+	public void testGetMass() {
+		assertEquals("Mass should be " + mass, mass, testComponent.getMass(), 1e-15);
+	}
+	
+	private static class TestComponent extends BaseComponent {
+		public TestComponent(double mass) {
+			this.mass = mass;
+		}
+	}
+}
