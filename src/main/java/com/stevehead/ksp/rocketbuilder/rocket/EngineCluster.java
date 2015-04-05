fa596d784344335aad7480566aec5c6ef0fb5a22
Steve Johnson <sjohnson0684@gmail.com>
2015-04-05 15:13:24 -0400

    EngineCluster class implementation.

--- src/main/java/com/stevehead/ksp/rocketbuilder/rocket/EngineCluster.java
+++ src/main/java/com/stevehead/ksp/rocketbuilder/rocket/EngineCluster.java
@@ -1,5 +1,29 @@
 package com.stevehead.ksp.rocketbuilder.rocket;
 
-public class EngineCluster {
+import com.stevehead.ksp.rocketbuilder.interfaces.Thrustable;
 
+public class EngineCluster extends BaseThruster {
+	/**
+	 * The engines that makes up the cluster.
+	 */
+	private final Thrustable[] engines;
+	
+	/**
+	 * @param engines		the engines that make up the cluster
+	 */
+	public EngineCluster(Thrustable... engines) {
+		super(calculateDryMass(engines), calculateMass(engines),
+				calculateThrust(engines),calculateIsp(engines),
+				combinePropellants(engines));
+		this.engines = engines;
+	}
+	
+	/**
+	 * The engines that makes up the cluster.
+	 * 
+	 * @return			the engines that make up the cluster
+	 */
+	public Thrustable[] getEngines() {
+		return engines;
+	}
 }
