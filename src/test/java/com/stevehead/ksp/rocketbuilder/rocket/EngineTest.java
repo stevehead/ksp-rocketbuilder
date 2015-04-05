ca75ba3396f891bf872650bb2bb251ef000d66da
Steve Johnson <sjohnson0684@gmail.com>
2015-04-05 14:31:09 -0400

    Engine tests for tweakscale.

--- src/test/java/com/stevehead/ksp/rocketbuilder/rocket/EngineTest.java
+++ src/test/java/com/stevehead/ksp/rocketbuilder/rocket/EngineTest.java
@@ -8,11 +8,11 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.stevehead.ksp.rocketbuilder.game.Mod;
-import com.stevehead.ksp.rocketbuilder.interfaces.Thrustable;
 
 public class EngineTest {
 	
 	private static Engine testEngine;
+	private static Engine testTweakScaleEngine;
 	private static final String name = "Test Engine";
 	private static final Mod mod = Mod.CUSTOM;
 	private static final double dryMass = 10000;
@@ -20,10 +20,12 @@ public class EngineTest {
 	private static final double thrust = 2000000;
 	private static final double isp = 340;
 	private static final double size = 2.5;
+	private static final double tweakScaleSize = 1.25;
 	
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 		testEngine = new Engine(name, mod, dryMass, mass, thrust, isp, size);
+		testTweakScaleEngine = testEngine.tweakScale(tweakScaleSize);
 	}
 	
 	@Before
@@ -70,5 +72,64 @@ public class EngineTest {
 		Engine.setIspScaler(newIspScaler);
 		double actualDeltaV = Engine.KERBIN_GRAVITY * isp * newIspScaler * Math.log(mass / dryMass);
 		assertEquals("Test engine dV should be " + actualDeltaV, actualDeltaV, testEngine.getDeltaV(), 1e-7);
+	}
+	
+	@Test
+	public void testTweakScaleDifferentScale() {
+		assertNotSame("Engine objects should be different", testEngine, testTweakScaleEngine);
+		assertNotEquals("Engine sizes should be different", testEngine.getSize(), testTweakScaleEngine.getSize(), 1e-7);
+	}
+	
+	@Test
+	public void testTweakScaleSameScale() {
+		Engine testSameScale = testEngine.tweakScale(size);
+		assertSame("Engine objects should be same", testEngine, testSameScale);
+		assertEquals("Engine sizes should be same", testEngine.getSize(), testSameScale.getSize(), 1e-7);
+	}
+	
+	@Test
+	public void testTweakScaleDryMass() {
+		double ratio = tweakScaleSize / size;
+		double massScale = Math.pow(ratio, Engine.MASS_EXPONENT);
+		
+		assertEquals("Dry mass should be scaled by " + massScale, dryMass * massScale,
+				testTweakScaleEngine.getDryMass(), 1e-7);
+	}
+	
+	@Test
+	public void testTweakScaleMass() {
+		double ratio = tweakScaleSize / size;
+		double massScale = Math.pow(ratio, Engine.MASS_EXPONENT);
+		
+		assertEquals("Mass should be scaled by " + massScale, mass * massScale,
+				testTweakScaleEngine.getMass(), 1e-7);
+	}
+	
+	@Test
+	public void testTweakScaleThrust() {
+		double ratio = tweakScaleSize / size;
+		double thrustScale = Math.pow(ratio, Engine.THRUST_EXPONENT);
+		
+		assertEquals("Thrust should be scaled by " + thrustScale, thrust * thrustScale,
+				testTweakScaleEngine.getThrust(), 1e-7);
+	}
+	
+	@Test
+	public void testTweakScaleDeltaV() {
+		assertEquals("Delta-V should be the same", testEngine.getDeltaV(),
+				testTweakScaleEngine.getDeltaV(), 1e-7);
+	}
+	
+	@Test
+	public void testTweakScaleTWR() {
+		double ratio = tweakScaleSize / size;
+		double massScale = Math.pow(ratio, Engine.MASS_EXPONENT);
+		double thrustScale = Math.pow(ratio, Engine.THRUST_EXPONENT);
+		double twrScale = thrustScale / massScale;
+		
+		assertEquals("Min TWR should be scaled by " + twrScale, testEngine.getMinTWR() * twrScale,
+				testTweakScaleEngine.getMinTWR(), 1e-7);
+		assertEquals("Max TWR should be scaled by " + twrScale, testEngine.getMaxTWR() * twrScale,
+				testTweakScaleEngine.getMaxTWR(), 1e-7);
 	}
 }
