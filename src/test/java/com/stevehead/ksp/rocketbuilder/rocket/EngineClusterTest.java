67ec491d42d6569fa1313fd2746752316c986f31
Steve Johnson <sjohnson0684@gmail.com>
2015-04-05 17:37:12 -0400

    Unit tests for EngineCluster.

---
+++ src/test/java/com/stevehead/ksp/rocketbuilder/rocket/EngineClusterTest.java
@@ -0,0 +1,64 @@
+package com.stevehead.ksp.rocketbuilder.rocket;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.stevehead.ksp.rocketbuilder.interfaces.Thrustable;
+
+public class EngineClusterTest {
+	
+	private static EngineCluster testEngineCluster;
+	private static final Thrustable testEngine1 = Engine.PreDefined.LV_T30_LIQUID_FUEL_ENGINE;
+	private static final Thrustable testEngine2 = Engine.PreDefined.LV_909_LIQUID_FUEL_ENGINE;
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		testEngineCluster = new EngineCluster(testEngine1, testEngine2);
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		Engine.setIspScaler(1);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		Engine.setIspScaler(1);
+	}
+
+	@Test
+	public void getMass() {
+		double expectedValue = testEngine1.getMass() + testEngine2.getMass();
+		assertEquals(expectedValue, testEngineCluster.getMass(), 1e-7);
+	}
+	
+	@Test
+	public void getDryMass() {
+		double expectedValue = testEngine1.getDryMass() + testEngine2.getDryMass();
+		assertEquals(expectedValue, testEngineCluster.getDryMass(), 1e-7);
+	}
+	
+	@Test
+	public void getThrust() {
+		double expectedValue = testEngine1.getThrust() + testEngine2.getThrust();
+		assertEquals(expectedValue, testEngineCluster.getThrust(), 1e-7);
+	}
+	
+	@Test
+	public void getIsp() {
+		double expectedValue = (testEngine1.getThrust() + testEngine2.getThrust()) / (testEngine1.getThrust() / testEngine1.getIsp() + testEngine2.getThrust() / testEngine2.getIsp());
+		assertEquals(expectedValue, testEngineCluster.getIsp(), 1e-7);
+	}
+	
+	@Test
+	public void getIspAfterScaleChange() {
+		Engine.setIspScaler(0.81);
+		double expectedValue = (testEngine1.getThrust() + testEngine2.getThrust()) / (testEngine1.getThrust() / testEngine1.getIsp() + testEngine2.getThrust() / testEngine2.getIsp());
+		assertEquals(expectedValue, testEngineCluster.getIsp(), 1e-7);
+	}
+
+}
