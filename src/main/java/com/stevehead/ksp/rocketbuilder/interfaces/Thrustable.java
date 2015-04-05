e3661a5fdbb817954287e70a53f20442d05cd42b
Steve Johnson <sjohnson0684@gmail.com>
2015-04-05 15:08:45 -0400

    Removed PropellantRequired references.

--- src/main/java/com/stevehead/ksp/rocketbuilder/interfaces/Thrustable.java
+++ src/main/java/com/stevehead/ksp/rocketbuilder/interfaces/Thrustable.java
@@ -7,7 +7,7 @@ package com.stevehead.ksp.rocketbuilder.interfaces;
  * 
  * @author Steve Johnson
  */
-public interface Thrustable extends Expendable, PropellantRequired {
+public interface Thrustable extends Expendable {
 	/**
 	 * The ASL surface gravity of the planet Kerbin, in m/s^2
 	 */
