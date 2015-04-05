3a7e80db37b1d6d0c947cd817765d4975fbcdf6f
Steve Johnson <sjohnson0684@gmail.com>
2015-04-05 13:44:28 -0400

    Moddable and Nameable interfaces.

---
+++ src/main/java/com/stevehead/ksp/rocketbuilder/interfaces/Moddable.java
@@ -0,0 +1,17 @@
+package com.stevehead.ksp.rocketbuilder.interfaces;
+
+import com.stevehead.ksp.rocketbuilder.game.Mod;
+
+/**
+ * Object that is moddable in the game.
+ * 
+ * @author Steve Johnson
+ */
+public interface Moddable {
+	/**
+	 * The game mod
+	 * 
+	 * @return		the mod
+	 */
+	Mod getMod();
+}
---
+++ src/main/java/com/stevehead/ksp/rocketbuilder/interfaces/Nameable.java
@@ -0,0 +1,15 @@
+package com.stevehead.ksp.rocketbuilder.interfaces;
+
+/**
+ * Nameable is for any object that has a String-based name.
+ * 
+ * @author Steve Johnson
+ */
+public interface Nameable {
+	/**
+	 * The name.
+	 * 
+	 * @return		the name
+	 */
+	String getName();
+}
