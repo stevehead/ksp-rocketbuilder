da7844be90e12feb9ed7d9636913d7e90e6cf115
Steve Johnson <sjohnson0684@gmail.com>
2015-04-06 22:27:13 -0400

    Stage class implementation.

--- src/main/java/com/stevehead/ksp/rocketbuilder/rocket/Stage.java
+++ src/main/java/com/stevehead/ksp/rocketbuilder/rocket/Stage.java
@@ -1,5 +1,29 @@
 package com.stevehead.ksp.rocketbuilder.rocket;
 
-public class Stage {
+import com.stevehead.ksp.rocketbuilder.interfaces.Massive;
 
+public class Stage extends BaseThruster {
+	
+	/**
+	 * @param payload		the payload
+	 * @param stacks		the stacks
+	 */
+	public Stage(Massive payload, Stack... stacks) {
+		super(
+				payload.getMass() + calculateDryMass(stacks),
+				payload.getMass() + calculateMass(stacks),
+				calculateThrust(stacks),
+				calculateThrust(stacks),
+				combinePropellants(stacks)
+				);
+	}
+	
+	/**
+	 * @param payload		the payload
+	 * @param stack			the stack
+	 * @param stackNumber	the number of times the stack is repeated
+	 */
+	public Stage(Massive payload, Stack stack, int stackNumber) {
+		this(payload, stack.duplicate(stackNumber));
+	}
 }
