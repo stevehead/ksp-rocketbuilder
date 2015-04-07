f02015d79eb78d0759f86a3795551a27c929df59
Steve Johnson <sjohnson0684@gmail.com>
2015-04-06 21:13:59 -0400

    Stack class implementation.

--- src/main/java/com/stevehead/ksp/rocketbuilder/rocket/Stack.java
+++ src/main/java/com/stevehead/ksp/rocketbuilder/rocket/Stack.java
@@ -1,5 +1,25 @@
 package com.stevehead.ksp.rocketbuilder.rocket;
 
-public class Stack {
+import com.stevehead.ksp.rocketbuilder.interfaces.Thrustable;
 
+/**
+ * Stack is a fuel tank and engine combo.
+ * 
+ * @author Steve Johnson
+ *
+ */
+public class Stack extends BaseThruster {
+	/**
+	 * @param fuelTank		the stack's fuel tank
+	 * @param engine		the stack's engine(s)
+	 */
+	public Stack(FuelTank fuelTank, Thrustable engine) {
+		super(fuelTank.getDryMass() + engine.getDryMass(),
+				fuelTank.getMass() + engine.getMass(), engine.getThrust(),
+				engine.getIsp(), combinePropellants(fuelTank, engine));
+		
+		if (fuelTank.getType() != engine.getType()) {
+			throw new IllegalArgumentException("Fuel tank and engines types must match.");
+		}
+	}
 }
