aff6f37d71abaf4fdb3ac3e380341b76101e2dd9
Steve Johnson <sjohnson0684@gmail.com>
2015-04-06 22:27:01 -0400

    Added method that makes duplicates of the stack.

--- src/main/java/com/stevehead/ksp/rocketbuilder/rocket/Stack.java
+++ src/main/java/com/stevehead/ksp/rocketbuilder/rocket/Stack.java
@@ -1,5 +1,7 @@
 package com.stevehead.ksp.rocketbuilder.rocket;
 
+import java.util.Arrays;
+
 import com.stevehead.ksp.rocketbuilder.interfaces.Thrustable;
 
 /**
@@ -14,11 +16,28 @@ public class Stack extends BaseThruster {
 	 * @param engine		the stack's engine(s)
 	 */
 	public Stack(FuelTank fuelTank, Thrustable engine) {
-		super(calculateDryMass(fuelTank, engine), calculateMass(fuelTank, engine),
-				engine.getThrust(), engine.getIsp(), combinePropellants(fuelTank, engine));
+		super(
+				calculateDryMass(fuelTank, engine),
+				calculateMass(fuelTank, engine),
+				engine.getThrust(),
+				engine.getIsp(),
+				combinePropellants(fuelTank, engine)
+				);
 		
 		if (fuelTank.getType() != engine.getType()) {
 			throw new IllegalArgumentException("Fuel tank and engines types must match.");
 		}
+	}
+	
+	/**
+	 * Duplicates the stack into an array
+	 * 
+	 * @param stackNumber		the number of stacks
+	 * @return					the array of stacks
+	 */
+	public Stack[] duplicate(int stackNumber) {
+		Stack[] stacks = new Stack[stackNumber];
+		Arrays.fill(stacks, this);
+		return stacks;
 	}
 }
